3fc21317630d10532e07b0488517d83b04621c41
akashtripathiiiii <akashtripathi0108@gmail.com>
2023-11-13 16:14:03 +0530

    initial comment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Posts from "./components/Posts";
+import Profile from "./components/Profile";
+import data from "./data.json";
+
+console.log(data);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Profile data={data} />
+      <Posts data={data} />
     </div>
   );
 }
